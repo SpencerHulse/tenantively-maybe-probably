08b457c4b8485a04cb2e5bf6f8909d7c40b26d20
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-03-23 12:53:50 -0500

    update

--- models/Property.js
+++ models/Property.js
@@ -4,59 +4,6 @@ const sequelize = require("../config/connection");
 class Property extends Model {}
 
 Property.init(
-<<<<<<< HEAD
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-          },
-        landlord_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'landlord',
-                key: 'id'
-            }
-        },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        zipCode: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        bedrooms: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        bathrooms: {
-            type: DataTypes.DECIMAL(10, 1),
-            allowNull: false,
-
-        },
-        rent: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-        },
-        utilities: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
-     },
-     {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'property'
-      }
-    
-=======
   {
     id: {
       type: DataTypes.INTEGER,
@@ -111,7 +58,6 @@ Property.init(
     underscored: true,
     modelName: "property",
   }
->>>>>>> 2b39359fffae9f20997be5dd85f538d6cb9628ed
 );
 
 module.exports = Property;
